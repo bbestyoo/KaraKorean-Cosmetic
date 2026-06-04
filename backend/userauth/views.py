a78098af42652af13f55c43fd48b684bd8cf14d4
code-bbob <officialbibhab@gmail.com>
2026-06-04 12:57:33 +0545

    fixed patch on userinfoview

--- backend/userauth/views.py
+++ backend/userauth/views.py
@@ -134,15 +134,11 @@ class UserInfoView(APIView):
   
   def patch(self,request):
     user = request.user
-    new_bio = request.data.get("bio")
-    serializer = UserInfoSerializer
-    if new_bio:
-      user.bio = new_bio
-      user.save()
-      return Response({"msg":"Bio Updated"}, status=status.HTTP_200_OK)
-    else:
-      return Response("msg: Something went wrong!", status=status.HTTP_400_BAD_REQUEST)
-    
+
+    serializer = UserInfoSerializer(user, data=request.data, partial=True)
+    serializer.is_valid(raise_exception=True)
+    serializer.save()
+    return Response(serializer.data, status=status.HTTP_200_OK)
 
 
 
